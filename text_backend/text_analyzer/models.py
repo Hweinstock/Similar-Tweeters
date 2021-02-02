76eaf914bc6820a04d8809e7b822122dc5c494e5
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2021-02-02 10:33:04 -0800

    start of TextObject Model

--- text_backend/text_analyzer/models.py
+++ text_backend/text_analyzer/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+import json
 # Create your models here.
+
+
+class TextObjectData(models.Model):
+    label = models.CharField(max_length=120)
+    author = models.TextField()
+
+    top_n_words = models.JSONField()
+    top_p_puncs = models.JSONField()
+    top_s_sents = models.JSONField()
+
+    indexed_word_set = models.JSONField()
+    indexed_punc_set = models.JSONField()
+
+    average_word_length = models.FloatField()
+    average_sent_length = models.FloatField()
+    average_top_n_sent_length = models.FloatField()
+
+    sentence_lengths = models.JSONField()
+    sentence_length_freq = models.JSONField()
 
 
 class ComparisonData(models.Model):
--- text_backend/text_analyzer/serializers.py
+++ text_backend/text_analyzer/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import ComparisonData
+from .models import ComparisonData, TextObjectData
 
 
 class CompSerializer(serializers.ModelSerializer):
     class Meta:
         model = ComparisonData
         fields = ('id', 'label', 'text1', 'text2', 'comp')
+
+
+class TextObjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TextObjectData
+        fields = ('id', 'label', 'author', 'top_n_words', 'top_p_puncs',
+                  'top_s_sents', 'indexed_word_set', 'indexed_punc_set',
+                  'average_word_length', 'average_sent_length', 'average_top_n_sent_length',
+                  'sentence_lengths', 'sentence_length_freq')
